656ed31c0cf5c030f01b170a9f04eb624df76e3b
Alim Kugotov <a.kugotov@andersenlab.com>
2021-11-19 00:31:03 +0300

    added bombs

--- Lab 5/Lab 5.cpp
+++ Lab 5/Lab 5.cpp
@@ -8,12 +8,27 @@
 
 using namespace std;
 
+struct Bomb {
+    int x = -1;
+    int y = -1;
+    int time = 2;
+};
+
+vector<Bomb> bombs;
+
 void init();
 void draw();
+void plant_bomb();
+void decrease_bomb_time();
+void blow_up(int x, int y);
+void blow_up(Bomb bomb);
+void reload();
 void up();
 void down();
 void left();
 void right();
+void win();
+void lose();
 
 #define HERO 24
 #define BOMB 15
@@ -21,10 +36,14 @@ void right();
 #define DONE 251
 #define BORDER 177
 #define AIM 176
+#define HEART 3
 
 #define MAX_X 40
 #define MAX_Y 10
 char matrix[MAX_Y][MAX_X];
+
+int lives = 3;
+int level = 1;
 
 // main aim to blow up
 int main_door_x = -1;
@@ -34,13 +53,6 @@ int main_door_y = -1;
 int star_x = 5 - 1;
 int star_y = 5 - 1;
 
-struct Bomb {
-    int x;
-    int y;
-    int time = 3;
-};
-
-queue<Bomb> bombs;
 
 int main()
 {
@@ -50,22 +62,21 @@ int main()
     }
     */
 
-    // queue<>
-    // todo: plant_bomb()
-    // todo: boom in 2 steps
-    // bomb or done
-
     init();
     draw();
     while (1) {
         char input = _getch();
-        if (_kbhit()) {
-            if (input == -32) {
-                input = _getch();
-            }
+        if (_kbhit() && input == -32) {
+           input = _getch();
         }
-//        this_thread::sleep_for(chrono::milliseconds((speed - 10) * 10));
         switch (input) {
+        case 82: // R
+        case 114: // r
+            reload();
+            break;
+        case 32: // space
+            plant_bomb();
+            break;
         case 72:
             up();
             break;
@@ -86,8 +97,11 @@ int main()
 
 void up() {
     char ch = matrix[star_y - 1][star_x];
-    if (star_y > 1 && ch != (char) AIM) {
-        matrix[star_y][star_x] = '.';
+    if (ch == '.') {
+        if (matrix[star_y][star_x] != (char) BOMB) {
+            matrix[star_y][star_x] = '.';
+        }
+        decrease_bomb_time();
         star_y--;
         matrix[star_y][star_x] = HERO;
         draw();
@@ -96,8 +110,11 @@ void up() {
 
 void down() {
     char ch = matrix[star_y + 1][star_x];
-    if (star_y + 2 < MAX_Y && ch != (char) AIM) {
-        matrix[star_y][star_x] = '.';
+    if (ch == '.') {
+        if (matrix[star_y][star_x] != (char) BOMB) {
+            matrix[star_y][star_x] = '.';
+        }
+        decrease_bomb_time();
         star_y++;
         matrix[star_y][star_x] = HERO;
         draw();
@@ -106,8 +123,11 @@ void down() {
 
 void left() {
     char ch = matrix[star_y][star_x - 1];
-    if (star_x > 1 && ch != (char) AIM) {
-        matrix[star_y][star_x] = '.';
+    if (ch == '.') {
+        if (matrix[star_y][star_x] != (char)BOMB) {
+            matrix[star_y][star_x] = '.';
+        }
+        decrease_bomb_time();
         star_x--;
         matrix[star_y][star_x] = HERO;
         draw();
@@ -116,8 +136,11 @@ void left() {
 
 void right() {
     char ch = matrix[star_y][star_x + 1];
-    if (star_x + 2 < MAX_X && ch != (char) AIM) {
-        matrix[star_y][star_x] = '.';
+    if (ch == '.') {
+        if (matrix[star_y][star_x] != (char)BOMB) {
+            matrix[star_y][star_x] = '.';
+        }
+        decrease_bomb_time();
         star_x++;
         matrix[star_y][star_x] = HERO;
         draw();
@@ -140,7 +163,7 @@ void init_borders() {
 }
 
 void init_aims() {
-    std:random_device rd; // obtain a random number from hardware
+    std::random_device rd; // obtain a random number from hardware
     std::mt19937 gen(rd()); // seed the generator
     std::uniform_int_distribution<> distrX(2, MAX_X - 2); // define the range
     std::uniform_int_distribution<> distrY(2, MAX_Y - 2); // define the range
@@ -156,7 +179,6 @@ void init_aims() {
     }
 }
 
-
 void init() {
     for (int i = 0; i < MAX_Y; i++) {
         for (int j = 0; j < MAX_X; j++) {
@@ -170,7 +192,8 @@ void init() {
 
 void draw() {
     system("cls");
-
+    
+    cout << "Level: " << level << "\t\t\t     " << string(lives, (char)HEART) << endl;
     for (int i = 0; i < MAX_Y; i++) {
         for (int j = 0; j < MAX_X; j++) {
             cout << matrix[i][j];
@@ -185,5 +208,98 @@ void draw() {
     cout << "←" << " \t " << "left" << endl;
 
     cout << "Esc \t exit" << endl;
-    cout << "Space \t Plant bomb" << endl;
+    cout << "R \t reload hero position (minus life)" << endl;
+    cout << "Space \t plant bomb" << endl;
+}
+
+void decrease_bomb_time() {
+    for (auto &b : bombs) {
+        b.time--;
+        if (b.time == 0) {
+            blow_up(b);
+        }
+    }
+}
+
+void check_to_blow_up(int x, int y) {
+    char ch = matrix[y][x];
+    if (ch == '.') {
+        matrix[y][x] = BOOM;
+    }
+    else if (ch == (char)BOMB) {
+        blow_up(x, y);
+    }
+    else if (ch == (char) AIM) {
+        matrix[y][x] = BOMB;
+    }
+    else if (ch == HERO) {
+        lives--;
+        if (lives == 0) lose();
+    }
+}
+
+void blow_up(int x, int y) {
+    for (auto& bomb : bombs) {
+        if (bomb.x = x && bomb.y == y) {
+            blow_up(bomb);
+        }
+    }
+}
+
+void blow_up(Bomb bomb) {
+    int x = bomb.x;
+    int y = bomb.y;
+    matrix[y][x] = BOOM;
+    for (int i = 0; i < 2; i++) {
+        if (x - i > 0) check_to_blow_up(x - i, y);
+        if (x + i < MAX_X - 1) check_to_blow_up(x + i, y);
+        if (y - i > 0) check_to_blow_up(x, y - i);
+        if (y + i < MAX_Y - 1) check_to_blow_up(x, y + i);
+    }
+}
+
+
+
+void plant_bomb() {
+    matrix[star_y][star_x] = BOMB;
+    Bomb bomb;
+    bomb.x = star_x;
+    bomb.y = star_y;
+    bombs.push_back(bomb);
+    draw();
+}
+
+void reload() {
+    lives--;
+    if (lives == 0) lose();
+    std::random_device rd; // obtain a random number from hardware
+    std::mt19937 gen(rd()); // seed the generator
+    std::uniform_int_distribution<> distrX(1, MAX_X - 2); // define the range
+    std::uniform_int_distribution<> distrY(1, MAX_Y - 2); // define the range
+
+    while (1) {
+        int x = distrX(gen);
+        int y = distrY(gen);
+        
+        if (matrix[y][x] == '.') {
+            matrix[star_y][star_x] = '.';
+            star_x = x;
+            star_y = y;
+            matrix[star_y][star_x] = HERO;
+            draw();
+            break;
+        }
+    }
+}
+
+void win() {
+    system("cls");
+    cout << "You win! :)" << endl;
+    exit(0);
+}
+
+void lose() {
+    system("cls");
+    cout << "You lose! :(" << endl;
+    exit(0);
 }
